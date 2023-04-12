6842e994795413230d07562367d18ec8426dc49d
Peter Lind (@KEA) <petl@kea.dk>
2023-04-12 11:20:57 +0200

    Ændret højde på den første søjle

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 "use strict";
+
+window.addEventListener("load", start); 
+
+function start() {
+  console.log("JavaScript kører");
+  
+  const bars = document.querySelectorAll(".bar");
+
+  const firstBar = bars[0];
+
+  const h = 50;
+
+  firstBar.style.height = `${h}px`;
+}
