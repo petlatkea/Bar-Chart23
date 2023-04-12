e80c85a5951fe646451e9a993d5b272a78bc48c5
Peter Lind (@KEA) <petl@kea.dk>
2023-04-12 11:34:06 +0200

    Tilføjet data i et array

--- script.js
+++ script.js
@@ -2,6 +2,8 @@
 
 window.addEventListener("load", start); 
 
+const data = [16, 30, 3, 29, 8, 18, 26, 5, 1, 31, 23, 6, 25, 17, 2, 10, 11, 20, 14, 15, 8, 0, 7, 4, 30, 22, 28, 11, 16, 9, 23, 27, 1, 7, 32, 31, 24, 19, 2, 13, 12];
+
 function start() {
   console.log("JavaScript kører");
   
